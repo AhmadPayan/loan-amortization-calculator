c7374d94ac89f5f857dd516d6d29d55bfe026555
Ahmad Payan <ahmadpayan71@gmail.com>
2023-09-18 21:22:23 +0300

    Add a page to use the calculator service and display the result

---
+++ src/backend/Pages/Index.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using backend.Dtos;
+using backend.Models;
+using backend.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace backend.Pages;
+
+[ValidateAntiForgeryToken]
+public class IndexModel : PageModel
+{
+    private readonly IAmortizationCalculator _amortizationCalculator;
+
+    public IndexModel(IAmortizationCalculator amortizationCalculator)
+    {
+        _amortizationCalculator = amortizationCalculator;
+    }
+
+    [BindProperty] public LoanRequestDto? Loan { get; set; }
+    public IEnumerable<PaymentSchedule>? AmortizationSchedule { get; set; }
+
+    public void OnGet()
+    {
+        SetPaymentFrequencies();
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            AmortizationSchedule = await _amortizationCalculator.CalculateAmortizationSchedule(Loan);
+        }
+
+        SetPaymentFrequencies();
+        return Page();
+    }
+
+    private void SetPaymentFrequencies()
+    {
+        var paymentFrequencies = new List<SelectListItem>()
+        {
+            new()
+            {
+                Text = "Monthly", Value = "1", Selected = true
+            }
+        };
+
+        ViewData["PaymentFrequencyData"] = paymentFrequencies;
+    }
+}
--- src/backend/Program.cs
+++ src/backend/Program.cs
@@ -1,6 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using backend.Services;
+using backend.Services.Contracts;
 
-app.MapGet("/", () => "Amortization Schedule Calculator");
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<IAmortizationCalculator, AmortizationCalculator>();
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.MapRazorPages();
+app.UseRouting();
 
 app.Run();
