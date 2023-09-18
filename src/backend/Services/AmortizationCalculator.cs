eb2a627ab4a9a04ffd6dae9b80c6045b0d9910f0
Ahmad Payan <ahmadpayan71@gmail.com>
2023-09-18 12:09:32 +0300

    Add a basic class for Actual Amortization Calculator

---
+++ src/backend/Services/AmortizationCalculator.cs
@@ -0,0 +1,15 @@
+﻿using backend.Dtos;
+using backend.Models;
+using backend.Services.Contracts;
+
+namespace backend.Services;
+
+public class AmortizationCalculator : IAmortizationCalculator
+{
+    public Task<List<PaymentSchedule>> CalculateAmortizationSchedule(LoanRequestDto loan)
+    {
+        if (loan is null) throw new NullReferenceException($"Parameter {nameof(loan)} cannot be NULL");
+        
+        return null;
+    }
+}
