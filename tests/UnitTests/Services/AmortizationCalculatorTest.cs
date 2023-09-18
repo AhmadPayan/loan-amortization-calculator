b8ded89afe78ca5ee3206b1fdfd0d47e4e9733fc
Ahmad Payan <ahmadpayan71@gmail.com>
2023-09-18 12:10:07 +0300

    Add Guard Against NULL input in AmortizationCalculator Service

---
+++ tests/UnitTests/Services/AmortizationCalculatorTest.cs
@@ -0,0 +1,14 @@
+﻿using backend.Services;
+
+namespace UnitTests.Services;
+
+public class AmortizationCalculatorTest
+{
+    [Fact]
+    public async Task CalculateAmortizationSchedule_InvalidInputProvided_ThrowsException()
+    {
+        var sut = new AmortizationCalculator();
+
+        await Assert.ThrowsAsync<NullReferenceException>(() => sut.CalculateAmortizationSchedule(null));
+    }
+}
